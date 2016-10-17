117ac64f43190726d82a6819385630144378856a
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2016-10-17 11:50:11 +0900

    ➕色を指定しやすくした

---
+++ Feel Distance/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  Feel Distance
+//
+//  Created by Tatsuro Ueda on 2016/10/17.
+//  Copyright © 2016年 Tatsuro Ueda. All rights reserved.
+//
+
+import UIKit
+
+class Color: NSObject {
+    /*
+    【Swift】classとstaticの挙動の違いを整理する - Qiita
+    http://qiita.com/shimesaba/items/dc976b3974cfb41bec0c
+    */
+    static let graph =
+        UIColor(red: 124.0 / 255.0, green: 161.0 / 255.0, blue: 255.0/255.0, alpha: 0.5)
+    static let scale =
+        UIColor(red: 229.0 / 255.0, green: 229.0 / 255.0, blue:         0.0, alpha: 1.0)
+}
