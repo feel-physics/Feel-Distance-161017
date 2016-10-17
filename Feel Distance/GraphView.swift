91335bb0936bf8e3d1e79dabf7b6bba501adcddf
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2016-10-17 11:49:49 +0900

    ➕ビュー、ビューコントローラを用意した

---
+++ Feel Distance/GraphView.swift
@@ -0,0 +1,16 @@
+//
+//  GraphView.swift
+//  Feel Distance
+//
+//  Created by Tatsuro Ueda on 2016/10/17.
+//  Copyright © 2016年 Tatsuro Ueda. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        backgroundColor = UIColor(white: 0, alpha: 0.5)
+    }
+}
---
+++ Feel Distance/GraphViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.swift
+//  Feel Distance
+//
+//  Created by Tatsuro Ueda on 2016/10/17.
+//  Copyright © 2016年 Tatsuro Ueda. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    
+}
