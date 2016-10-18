cbc845ef972e3aaf30c4a9528648b1d117414171
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2016-10-18 09:23:26 +0900

    🐛ストーリーボードの修正

--- Feel Distance/PreviewViewController.swift
+++ Feel Distance/PreviewViewController.swift
@@ -11,8 +11,6 @@ import AVFoundation
 
 class PreviewViewController: UIViewController {
     
-    @IBOutlet var previewView: UIView!
-    
     /*
     ios - StoryBoardで配置したUICollectionViewのサイズ動的変更について - スタック・オーバーフロー
     http://ja.stackoverflow.com/questions/10098/storyboard%E3%81%A7%E9%85%8D%E7%BD%AE%E3%81%97%E3%81%9Fuicollectionview%E3%81%AE%E3%82%B5%E3%82%A4%E3%82%BA%E5%8B%95%E7%9A%84%E5%A4%89%E6%9B%B4%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6
@@ -29,10 +27,10 @@ class PreviewViewController: UIViewController {
             //画像を表示するレイヤーを生成
             let captureVideoLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer.init(session: captureSession)
             captureVideoLayer.connection.videoOrientation     = AVCaptureVideoOrientation.LandscapeRight
-            captureVideoLayer.frame        = previewView.frame
+            captureVideoLayer.frame        = view.frame
             captureVideoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
             //Viewに背景として追加
-            previewView.layer.insertSublayer(captureVideoLayer, atIndex: 0)
+            view.layer.insertSublayer(captureVideoLayer, atIndex: 0)
             
             captureSession.startRunning()
         }
