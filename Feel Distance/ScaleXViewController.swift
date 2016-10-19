ef9158a68112551c694ea9def5fb27e7b1d03794
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2016-10-19 10:13:13 +0900

    🎨View.swiftファイルを削除してストーリーボードに移した

---
+++ Feel Distance/ScaleXViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ScaleXViewController.swift
+//  Feel Distance
+//
+//  Created by Tatsuro Ueda on 2016/10/19.
+//  Copyright © 2016年 Tatsuro Ueda. All rights reserved.
+//
+
+import Foundation
